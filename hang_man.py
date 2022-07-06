5fad52a8c3fbeaf8c7c9a5e01a591a08757a119e
anthony.phan94 <anthony.phan94@outlook.comgit push -u origin main>
2022-07-05 22:58:39 -0500

    add function for games

--- hang_man.py
+++ hang_man.py
@@ -1,14 +1,41 @@
-myWord = input('Enter a word for Game:')
-print(myWord)
+# # myWord = input('Enter a word for Game:')
+# # print(myWord)
+from tkinter import W
+
+
+myWord = 'computerc'
 wordList = list(myWord)
+life = 6 
+print(wordList)
 
-print(wordList) 
-print(len(wordList))
 
-emptyList = []
+displayList = []
 for i in range(len(wordList)):
-    emptyList.append('_')
+    displayList.append('_')
 
-print(emptyList)
+print(displayList)
 
+
+end_of_game = False
+while not end_of_game:
+
+    guessWord = input('Please Enter your guess: ')
+    for i in range(len(wordList)):
+        letter = wordList[i]
+        if guessWord == letter:
+            displayList[i] = letter
+
+    # to check word not in wordList, has to stay outside of for loop
+    # otherwise, it will print out many word in the list
+    if guessWord not in wordList:
+        life -= 1
+    if life == 0:
+        end_of_game = True
+        print('you lost')
+
+    print(displayList)
+
+    if "_" not in displayList:
+        end_of_game = True   
+        print('win')
 
